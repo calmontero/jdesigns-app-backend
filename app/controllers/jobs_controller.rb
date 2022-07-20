f7ba91f3a01c73efac9926aaceeec6fd6223d0a3
calmontero <carlos.alfonso.montero@gmail.com>
2022-07-20 19:01:55 -0400

    added controller Job and Testimonial

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,42 @@
 class JobsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     def index
         jobs = Job.all
         render json: jobs, except: [:created_at, :updated_at]
     end
 
+    def create
+        job = Job.create!(job_params)
+        render json: job, status: :created
+    end
+
+    def update
+        job = find_job
+        job.update!(job_params)
+        render json: job
+    end
+
+    def destroy
+        job = job.destroy(find_job)
+        head :no_content
+    end
+
     private
+    def job_params
+        params.permit(:title)
+    end
+
+    def find_job
+        Job.find(params[:id])
+    end
+
     def render_not_found_response
         render json: { error: "job not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,2 +1,42 @@
 class TestimonialsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        testimonials = Testimonial.all
+        render json: testimonials, except: [:created_at, :updated_at]
+    end
+
+    def create
+        testimonial = Testimonial.create!(testimonial_params)
+        render json: testimonial, status: :created
+    end
+
+    def update
+        testimonial = find_testimonial
+        testimonial.update!(testimonial_params)
+        render json: testimonial
+    end
+
+    def destroy
+        testimonial = testimonial.destroy(find_testimonial)
+        head :no_content
+    end
+
+    private
+    def testimonial_params
+        params.permit(:account, :comment, :image, :link)
+    end
+
+    def find_testimonial
+        testimonial.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "testimonial not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
